c52e5b14e74c8fd16c3d4a4a683fa688a61f6e79
Scott <scott@scottharwood.dev>
2023-12-02 17:21:46 +0000

    initial

---
+++ 02/main.py
@@ -0,0 +1,58 @@
+from argparse import ArgumentParser
+from collections import defaultdict
+import re;
+
+inputSet = {
+    'red': 12,
+    'green': 13,
+    'blue': 14
+}
+
+
+def handleLine(line: str):
+
+    game, rest = line.split(":")
+    gameId = int(game.split(" ")[-1])
+
+    groups = rest.split(";")
+
+    maxPerColour = defaultdict(lambda: 0)
+
+    for group in groups:
+        groupColours = re.findall('(\d+) ((?:red)|(?:green)|(?:blue))', group)
+        for count, colour in groupColours:
+            count = int(count)
+            if maxPerColour[colour] < count:
+                maxPerColour[colour] = count
+
+
+    for colour in maxPerColour:
+        if maxPerColour[colour] > inputSet[colour]:
+            return 0
+    return gameId
+
+def main(file: str):
+    lines = open(file).readlines()
+
+    count = 0
+    for line in lines:
+        count += handleLine(line)
+    return count
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--source", default="givensample.txt")
+    parser.add_argument("--target", type=int)
+
+
+    args = parser.parse_args()
+
+    res = main(args.source)
+
+    if args.target:
+        if args.target != res:
+            print(f"failed, got {res} expected {args.target}")
+        else:
+            print("correct")
+    else:
+        print("Result:", res)
